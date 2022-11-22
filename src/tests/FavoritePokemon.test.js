67736d70836283a0158ebda42882fde18aa2c083
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-11-22 16:10:55 -0300

    req 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import FavoritePokemon from '../pages/FavoritePokemon';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente Favorite Pokémon', () => {
+  it('teste se a mensagem No favorite pokemon found é exibida caso não exista nenhum pokémon favoritado', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemon />);
+    const titleEl = getByText(/no favorite pokémon found/i);
+    expect(titleEl).toBeInTheDocument();
+  });
+});
