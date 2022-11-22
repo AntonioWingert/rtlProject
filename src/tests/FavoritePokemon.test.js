843fbb7f8be0c27f5436d86db6777c535e7b42b7
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-11-22 17:21:50 -0300

    req 5

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,3 +1,4 @@
+import pokemonList from '../data';
 import FavoritePokemon from '../pages/FavoritePokemon';
 import renderWithRouter from '../renderWithRouter';
 
@@ -6,5 +7,17 @@ describe('Testa o componente Favorite Pokémon', () => {
     const { getByText } = renderWithRouter(<FavoritePokemon />);
     const titleEl = getByText(/no favorite pokémon found/i);
     expect(titleEl).toBeInTheDocument();
+  });
+  it('Teste se são exibidos todos os cards de Pokémon favoritados', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemon
+      pokemonList={ pokemonList }
+    />);
+    expect(getByText(/pikachu/i)).toBeInTheDocument();
+    expect(getByText(/Charmander/i)).toBeInTheDocument();
+    expect(getByText(/Caterpie/i)).toBeInTheDocument();
+    expect(getByText(/Ekans/i)).toBeInTheDocument();
+    expect(getByText(/Alakazam/i)).toBeInTheDocument();
+    expect(getByText(/Snorlax/i)).toBeInTheDocument();
+    expect(getByText(/Dragonair/i)).toBeInTheDocument();
   });
 });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,67 @@
-test('', () => {});
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testa o componente Pokedex.js', () => {
+  it('testa se a pagina contem um H2 com o texto Encountered Pokémon', () => {
+    renderWithRouter(<App />);
+
+    const titleEl = screen.getByRole('heading', { name: /encountered pokémon/i, level: 2 });
+
+    expect(titleEl).toBeInTheDocument();
+  });
+  it('teste se é exibido o proximo pokémon da lista ao clicar no botão', async () => {
+    renderWithRouter(<App />);
+    const pokemonName = await screen.findByText(/pikachu/i);
+    const button = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Charmander');
+    });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Caterpie');
+    });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Ekans');
+    });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Alakazam');
+    });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Mew');
+    });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Rapidash');
+    });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Snorlax');
+    });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Dragonair');
+    });
+    userEvent.click(button);
+    await waitFor(() => {
+      expect(pokemonName).toHaveTextContent('Pikachu');
+    });
+  });
+  it('teste se a pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+    const button = screen.getByRole('button', { name: /all/i });
+    expect(button).toBeInTheDocument();
+
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    userEvent.click(buttons[0]);
+    expect(buttons[0]).toHaveTextContent('Electric');
+
+    userEvent.click(button);
+    expect(button).toBeVisible();
+  });
+});
